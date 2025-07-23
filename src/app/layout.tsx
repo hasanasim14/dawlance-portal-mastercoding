0cea5b720ad3e9e26b20c0233ff57ba94a418b73
Hasan Aasim <hasan.asim14@gmail.com>
2025-07-23 17:41:53 +0500

    file configs changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dawlance MasterCoder Application",
+  title: "Dawlance Analytics Application",
   description: "",
 };
 
